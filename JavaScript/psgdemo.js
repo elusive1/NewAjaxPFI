93cc0a24bc020e00b13e4b9505c3033924d82f7a
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-12 10:56:50 +0000

    Adding javascript to html site externally

---
+++ JavaScript/psgdemo.js
@@ -0,0 +1,7 @@
+/*******   JavaScript created by Zeshaan Nawaz. ********/
+
+//Hide the powerade GIF when buy now is clicked.
+document.getElementById('Merchantbutton').onclick = function() {
+    document.getElementById('powerade').style.display = 'none';
+  }
+
