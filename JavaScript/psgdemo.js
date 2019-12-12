77150007913be8a5dbf1cd51de88735203584c6e
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-12 12:50:39 +0000

    changed level of JS function

--- JavaScript/psgdemo.js
+++ JavaScript/psgdemo.js
@@ -1,5 +1,10 @@
 /*******   JavaScript created by Zeshaan Nawaz. ********/
 
+//Hide the powerade GIF when buy now is clicked.
+document.getElementById('Merchantbutton').onclick = function() {
+    document.getElementById('powerade').style.display = 'none';
+  }
+
 //trying out merchantcall on page load
 var hidebutton = "hidebutton";
 var serviceId = "875";
@@ -8,9 +13,3 @@ var contentUrl = "/index.html";
 var contentId = "sub1";
 
 document.getElementById('Merchantbutton2').onload = merchantCall(hidebutton, serviceId, reference, contentUrl, contentId);
-
-//Hide the powerade GIF when buy now is clicked.
-document.getElementById('Merchantbutton').onclick = function() {
-    document.getElementById('powerade').style.display = 'none';
-  }
-
