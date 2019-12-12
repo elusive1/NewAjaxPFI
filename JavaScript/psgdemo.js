5819148506410f8d4670498bbec4d250de6aacba
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-12 12:10:45 +0000

    new sub2 page big changes

--- JavaScript/psgdemo.js
+++ JavaScript/psgdemo.js
@@ -1,5 +1,15 @@
 /*******   JavaScript created by Zeshaan Nawaz. ********/
 
+//trying out merchantcall on page load
+let MerchantCall = function(hidebutton, serviceId, reference, contentUrl, contentId){
+    hidebutton = "hidebutton";
+    serviceId = "875";
+    reference = "powerade";
+    contentUrl = "/index.html";
+    contentId = "sub1";
+}
+document.getElementById('Merchantbutton2').onload = MerchantCall();
+
 //Hide the powerade GIF when buy now is clicked.
 document.getElementById('Merchantbutton').onclick = function() {
     document.getElementById('powerade').style.display = 'none';
