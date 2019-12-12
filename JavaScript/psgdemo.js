db3e65b2cb9a253708d280ec9bef7d9bc265ea19
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-12 12:20:35 +0000

    new merchantcall values

--- JavaScript/psgdemo.js
+++ JavaScript/psgdemo.js
@@ -1,13 +1,12 @@
 /*******   JavaScript created by Zeshaan Nawaz. ********/
 
 //trying out merchantcall on page load
-let MerchantCall = function(hidebutton, serviceId, reference, contentUrl, contentId){
-    hidebutton = "hidebutton";
-    serviceId = "875";
-    reference = "powerade";
-    contentUrl = "/index.html";
-    contentId = "sub1";
-}
+var hidebutton = "hidebutton";
+var serviceId = "875";
+var reference = "powerade";
+var contentUrl = "/index.html";
+var contentId = "sub1";
+
 document.getElementById('Merchantbutton2').onload = MerchantCall(hidebutton, serviceId, reference, contentUrl, contentId);
 
 //Hide the powerade GIF when buy now is clicked.
