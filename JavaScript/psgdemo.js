b93a2cf614655a698f111ff2c352732a04408757
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-12 11:28:39 +0000

    amended js again

--- JavaScript/psgdemo.js
+++ JavaScript/psgdemo.js
@@ -1,8 +1,7 @@
 /*******   JavaScript created by Zeshaan Nawaz. ********/
 
 //Hide the powerade GIF when buy now is clicked.
-let hideGif = function() {
+document.getElementById('Merchantbutton').onclick = function() {
     document.getElementById('powerade').style.display = 'none';
   }
-document.getElementById('Merchantbutton').onclick = hideGif();
 
