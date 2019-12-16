acc3afe369fc05d7dd5ef286b8536de8ea2256f9
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-16 11:26:37 +0000

    editing CSS + JS for button3 div

--- staging/JavaScript/psgdemo3.js
+++ staging/JavaScript/psgdemo3.js
@@ -5,6 +5,6 @@ var hidebutton = "hidebutton";
 var serviceId = "1577";
 var reference = "subfreetrial";
 var contentUrl = "/success.html";
-var contentId = "sub3";
+var contentId = "sub3Staging";
 
 document.getElementById('Merchantbutton2').onload = merchantCall(hidebutton, serviceId, reference, contentUrl, contentId);
