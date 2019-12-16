0f7ebc5ee23de98411a0ae48481765cc51d8c6d9
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-16 10:54:17 +0000

    adding few more PFI Ajax subscription pages

--- JavaScript/psgdemo3.js
+++ JavaScript/psgdemo3.js
@@ -2,9 +2,9 @@
 
 //merchantcall on page load
 var hidebutton = "hidebutton";
-var serviceId = "891";
-var reference = "powerade";
+var serviceId = "893";
+var reference = "subfreetrial";
 var contentUrl = "/success.html";
-var contentId = "sub2";
+var contentId = "sub3";
 
 document.getElementById('Merchantbutton2').onload = merchantCall(hidebutton, serviceId, reference, contentUrl, contentId);
