d2ba603d800bb61de229ad216ad40a64ba39b3b2
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-16 11:21:19 +0000

    adding a staging section

---
+++ staging/JavaScript/psgdemo3.js
@@ -0,0 +1,10 @@
+/*******   JavaScript created by Zeshaan Nawaz. ********/
+
+//merchantcall on page load
+var hidebutton = "hidebutton";
+var serviceId = "1577";
+var reference = "subfreetrial";
+var contentUrl = "/success.html";
+var contentId = "sub3";
+
+document.getElementById('Merchantbutton2').onload = merchantCall(hidebutton, serviceId, reference, contentUrl, contentId);
