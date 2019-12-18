6369899cb0c17c46d72e362b2ea77c0348e3f7fc
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-18 12:19:41 +0000

    amending JS script

--- staging/JavaScript/psgdemo3.js
+++ staging/JavaScript/psgdemo3.js
@@ -4,7 +4,7 @@
 var hidebutton = "hidebutton";
 var serviceId = "1577";
 var reference = "subfreetrial";
-var contentUrl = "/success.html";
+var contentUrl = "/NewAjaxPFI/success.html";
 var contentId = "sub3Staging";
 
 document.getElementById('Merchantbutton2').onload = merchantCall(hidebutton, serviceId, reference, contentUrl, contentId);
