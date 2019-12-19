323aa071f928059fb51fb5689987b225d98a52b6
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-19 16:13:08 +0000

    changed location ref for redirect

--- JavaScript/psgsuccess.js
+++ JavaScript/psgsuccess.js
@@ -1,7 +1,7 @@
 /*******   JavaScript created by Zeshaan Nawaz. ********/
 
 setTimeout(function () {
-    window.location.href = "pfi.psgdemo.com/NewAjaxPFI/index.html"; //will redirect to main index page.
+    window.location.href = "http://pfi.psgdemo.com/NewAjaxPFI/index.html"; //will redirect to main index page.
  }, 10000); //timeout set to redirect after 10 seconds/
 
  document.getElementById('headertext').onload = setTimeout();
