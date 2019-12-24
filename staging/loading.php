2c012faa2ce110cc5bff2f7cb4e0884b8cce6644
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-24 12:47:15 +0000

    adding a loading page and gif for one step before content site

---
+++ staging/loading.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" charset="UTF-8" content="ie=edge">
+<link rel="stylesheet" href="http://staging.psgdemo.com\NewAjaxPFI\staging\css\Style.css">
+</head>
+<body>
+<br>
+<div class="powerade">
+<div id="powerade">
+<img src="http://staging.psgdemo.com\NewAjaxPFI\staging\images\loading.gif">
+</div>
+<br>
+<br>
+<div class ="footer">
+</div>
+</body>
+</html>
