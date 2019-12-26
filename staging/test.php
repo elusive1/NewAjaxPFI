d59f181d94c24d01ac7f751f17dab4dd2896e973
Zeshaan <zeshaan.nawaz1@gmail.com>
2019-12-26 16:06:23 +0000

    changing the qoutation marks for the getrespponseheader value

--- staging/test.php
+++ staging/test.php
@@ -91,7 +91,7 @@ echo "<script type=\"text/javascript\"> var inproctest = function() { console.lo
    $.ajax({
       url:'http://staging.psgdemo.com/NewAjaxPFI/staging/inproc.php'
     }).done(function (data, textStatus, xhr) { 
-      console.log(xhr.getResponseHeader('HTTP_X_PFI_STATUS')); 
+      console.log(xhr.getResponseHeader("HTTP_X_PFI_STATUS")); 
    });
     //
 </script>
