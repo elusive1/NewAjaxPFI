b6bdea5ad6be58ea91776287957002c3268c2b62
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-01-06 15:34:45 +0000

    re adding the staging script back in

--- staging/loading.php
+++ staging/loading.php
@@ -1,7 +1,14 @@
 <?php
 header("X-PFI-InProcessUrl: staging.psgdemo.com/NewAjaxPFI/staging/inproc.php");
 
-echo $_GET["name"];
+//echo $_GET["test"];
+
+function get_contents() {
+    file_get_contents("http://staging.psgdemo.com/NewAjaxPFI/staging/inproc.php");
+    var_dump($http_response_header);
+    echo var_dump($http_response_header);
+  }
+  get_contents();
 
 ?>
 <html>
