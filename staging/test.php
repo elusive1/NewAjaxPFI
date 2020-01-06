f0af9a7609ac928ae936974b81bd42262efc9a88
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-01-06 11:46:27 +0000

    adding the capture to the output of print_r function

--- staging/test.php
+++ staging/test.php
@@ -68,10 +68,10 @@ echo "<script type=\"text/javascript\"> var inproctest = function() { console.lo
     //echo"<br>";
     //echo"<br>";
     echo"response header ";
-    echo print_r($http_response_header);
+    echo print_r($http_response_header, true);
     echo"<br>";
     echo"Request ";
-    echo print_r($_REQUEST[4]);
+    echo print_r($_REQUEST['HTTP_X_PFI_STATUS'], true);
     echo"<br>";
     echo"<br>";
     $url = 'http://staging.psgdemo.com/NewAjaxPFI/staging/inproc.php';
