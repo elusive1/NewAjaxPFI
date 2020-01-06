21c71bdac7e705e3c14d65f0a4f6d70701ade45c
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-01-06 15:06:15 +0000

    adding echo within the function

--- staging/loading.php
+++ staging/loading.php
@@ -1,10 +1,9 @@
 <?php
 function get_contents() {
     file_get_contents("http://staging.psgdemo.com/NewAjaxPFI/staging/inproc.php");
-    var_dump($http_response_header);
+    echo var_dump($http_response_header);
   }
   get_contents();
-  echo print_r($http_response_header);
 ?>
 <html>
 <head>
