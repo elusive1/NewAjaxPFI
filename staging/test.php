b0f2d7d425bd2ebf3abf84f3ec7a740069ac3ed4
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-01-06 17:38:12 +0000

    adding jquery now

--- staging/test.php
+++ staging/test.php
@@ -101,7 +101,7 @@ echo "<script type=\"text/javascript\"> var inproctest = function() { console.lo
 
 ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+<script>
 $("#footer").load(location.href + " #footer");
 </script>
 
