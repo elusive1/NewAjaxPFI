1e5ee856676899780b52f380a9093ff98b062b2b
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-01-06 16:49:07 +0000

    re-adding file_get_contents

--- staging/loading.php
+++ staging/loading.php
@@ -3,10 +3,11 @@ header("X-PFI-InProcessUrl: staging.psgdemo.com/NewAjaxPFI/staging/inproc.php");
 
 //echo $_GET["test"];
 
-function replicate_sessiontoken() {
+function get_contents() {
+    file_get_contents("http://staging.psgdemo.com/NewAjaxPFI/staging/inproc.php");
     //var_dump($http_response_header);
     //echo var_dump($http_response_header[5]['"']);
-    echo "<br>" . var_dump($http_response_header[5]);
+    //echo "<br>" . var_dump($http_response_header[5]);
     foreach($http_response_header as $key=> $value)
 {
   
@@ -14,7 +15,7 @@ function replicate_sessiontoken() {
 }
   }
   
-replicate_sessiontoken();
+  get_contents();
 
 ?>
 <html>
