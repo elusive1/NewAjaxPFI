53cfb98de7ba4b9874d2178b87ccdf403f9f2604
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-03-05 11:58:10 +0000

    amending the JS

--- TAP2BILL_V2/js/main.js
+++ TAP2BILL_V2/js/main.js
@@ -318,14 +318,13 @@
 		tabs();
 	});
 
+	function myFunction() {
+		var x = document.getElementById("pricing");
+		if (x.style.display === "none") {
+		  x.style.display = "block";
+		} else {
+		  x.style.display = "none";
+		}
+	  }
 
 }());
-
-function myFunction() {
-	var x = document.getElementById("pricing");
-	if (x.style.display === "none") {
-	  x.style.display = "block";
-	} else {
-	  x.style.display = "none";
-	}
-  }
