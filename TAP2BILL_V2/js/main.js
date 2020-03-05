04e96c37fbde4237b469d2eed42d2cb4447e9580
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-03-05 12:08:11 +0000

    adding in JQuery for buttons

--- TAP2BILL_V2/js/main.js
+++ TAP2BILL_V2/js/main.js
@@ -318,13 +318,4 @@
 		tabs();
 	});
 
-	function myFunction() {
-		var x = document.getElementById("pricing");
-		if (x.style.display === "none") {
-		  x.style.display = "block";
-		} else {
-		  x.style.display = "none";
-		}
-	  }
-
 }());
