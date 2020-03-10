811c7388e38a564c03c3d6beda2b7940f1ea7052
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-03-10 12:40:22 +0000

    amending the button config via JS

--- TAP2BILL_V2/js/div-swapout.js
+++ TAP2BILL_V2/js/div-swapout.js
@@ -1,17 +1,18 @@
 /* ZN - swap out the  */
 
 document.getElementById("PSMS").onclick = function() { 
-    
+
     document.getElementById("dcb").style.display = "none";
     document.getElementById("psms").style.display = "block";
 } 
-/*
-document.getElementById("rounded").onclick = function() { 
 
-    document.getElementById("rounded").style.display = "none"; 
+document.getElementById("DCB").onclick = function() { 
+
+    document.getElementById("psms").style.display = "none";
+    document.getElementById("dcb").style.display = "block"; 
 
 } 
-
+/*
 document.getElementById("square").onclick = function() { 
 
     document.getElementById("square").style.display = "none"; 
