225fa693c894c29dc669300bc0d93247572b6891
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-03-10 12:26:02 +0000

    adding a styling for div-swapouts before executing

---
+++ TAP2BILL_V2/js/div-swapout.js
@@ -0,0 +1,17 @@
+document.getElementById("circle").onclick = function() { 
+  
+    document.getElementById("circle").style.display = "none"; 
+
+} 
+
+document.getElementById("rounded").onclick = function() { 
+
+    document.getElementById("rounded").style.display = "none"; 
+
+} 
+
+document.getElementById("square").onclick = function() { 
+
+    document.getElementById("square").style.display = "none"; 
+
+} 
