370b40e5fcf2d50025798622b62d3b5b8abf1dc5
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-03-10 12:45:55 +0000

    adding in a third section, with adding CSS + JS

--- TAP2BILL_V2/js/div-swapout.js
+++ TAP2BILL_V2/js/div-swapout.js
@@ -3,18 +3,22 @@
 document.getElementById("PSMS").onclick = function() { 
 
     document.getElementById("dcb").style.display = "none";
+    document.getElementById("api").style.display = "none";
     document.getElementById("psms").style.display = "block";
 } 
 
 document.getElementById("DCB").onclick = function() { 
 
     document.getElementById("psms").style.display = "none";
+    document.getElementById("api").style.display = "none";
     document.getElementById("dcb").style.display = "block"; 
 
 } 
-/*
-document.getElementById("square").onclick = function() { 
 
-    document.getElementById("square").style.display = "none"; 
+document.getElementById("API").onclick = function() { 
 
-} */
+    document.getElementById("psms").style.display = "none";
+    document.getElementById("dcb").style.display = "none";
+    document.getElementById("api").style.display = "block";  
+
+}
