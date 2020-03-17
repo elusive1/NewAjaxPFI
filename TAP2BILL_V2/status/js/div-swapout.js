4e4bba338d00338236e902865230b35714447784
Zeshaan <zeshaan.nawaz1@gmail.com>
2020-03-17 16:11:20 +0000

    adding a status holding page

---
+++ TAP2BILL_V2/status/js/div-swapout.js
@@ -0,0 +1,71 @@
+/* ZN - swap out the divs on the how-to section */
+
+document.getElementById("PSMS").onclick = function() { 
+
+    document.getElementById("dcb").style.display = "none";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("psms").style.display = "block";
+};
+
+document.getElementById("DCB").onclick = function() { 
+
+    document.getElementById("psms").style.display = "none";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("dcb").style.display = "block"; 
+
+};
+
+document.getElementById("API").onclick = function() { 
+
+    document.getElementById("psms").style.display = "none";
+    document.getElementById("dcb").style.display = "none";
+    document.getElementById("api").style.display = "block";  
+
+};
+
+// For the services button on the menu
+function servicesMenu() {
+    window.location.replace("http://tap2bill.psgdemo.com");
+    var section = document.getElementById("js-services");
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'});
+};
+
+// For the about-us section on the menu
+function AboutusMenu() {
+    var section = document.getElementById("fh5co-pricing");
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'});
+};
+
+// For the about-us section on the menu
+function Partners() {
+    window.location.replace("http://tap2bill.psgdemo.com");
+    var section = document.getElementById("fh5co-testimonial");
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'});
+};
+
+// For the More section on the DCB section
+function MoreDCB() {
+    var section = document.getElementById("fh5co-schedule");
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    document.getElementById("psms").style.display = "none";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("dcb").style.display = "block";
+};
+
+// For the More section on the PSMS section
+function MorePSMS() {
+    var section = document.getElementById("fh5co-schedule");
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    document.getElementById("dcb").style.display = "none";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("psms").style.display = "block";
+};
+
+// For the More section on the API section
+function MoreAPI() {
+    var section = document.getElementById("fh5co-schedule");
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    document.getElementById("psms").style.display = "none";
+    document.getElementById("dcb").style.display = "none";
+    document.getElementById("api").style.display = "block";  
+};
